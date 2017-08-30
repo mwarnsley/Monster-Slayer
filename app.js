8c34d1630b8dcfc9aca5a7e81dcaa8495d9ce5fa
Marcus Warnsley <marcus@PC0159-Jscott.local>
2017-08-30 14:25:29 -0400

    Editing the attack function for the player and monster health

--- app.js
+++ app.js
@@ -11,5 +11,30 @@ new Vue({
       this.playerHealth = 100;
       this.monsterHealth = 100;
     },
+    attack: function() {
+      var max = 10;
+      var min = 3;
+      var damage = Math.max(Math.floor(Math.random() * max) + 1);
+      this.monsterHealth -= damage;
+
+      if (this.monsterHealth <= 0) {
+        alert('You have won');
+        this.gameIsRunning = false;
+        return;
+      }
+
+      max = 12;
+      min = 5;
+      damage = Math.max(Math.floor(Math.random() * max) + 1);
+      this.playerHealth -= damage;
+
+      if (this.playerHealth <= 0) {
+        alert('You have lost!');
+        this.gameIsRunning = false;
+      }
+    },
+    specialAttack: function() {},
+    heal: function() {},
+    giveUp: function() {},
   },
 });
